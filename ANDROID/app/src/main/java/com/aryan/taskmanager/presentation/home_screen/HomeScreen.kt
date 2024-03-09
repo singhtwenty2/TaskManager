36d9051ee71f11514e5257bda88671333d6efe6b
Aryan Singh <125189374+mrysg@users.noreply.github.com>
2024-03-09 11:10:56 +0530

    Added Delete Route

--- ANDROID/app/src/main/java/com/aryan/taskmanager/presentation/home_screen/HomeScreen.kt
+++ ANDROID/app/src/main/java/com/aryan/taskmanager/presentation/home_screen/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.aryan.taskmanager.presentation.home_screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,16 +12,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -63,21 +67,28 @@ fun HomeScreenComposable(
             modifier = Modifier.fillMaxSize()
         ) {
             items(tasks) { task ->
-                TaskItem(task = task) {
-                    /**
-                     * NAVIGATION WITH ARGS
-                     */
-                    navHostController.navigate(
-                        route = "${NavigationRoute.UPDATETASK.route}/${task.id}"
-                    )
-                }
+                TaskItem(
+                    task = task,
+                    onItemClick = {
+                        navHostController.navigate(
+                            route = "${NavigationRoute.UPDATETASK.route}/${task.id}"
+                        )
+                    },
+                    onDeleteClick = {
+                        viewModel.deleteTask(task.id)
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun TaskItem(task: TaskResponse, onItemClick: () -> Unit) {
+fun TaskItem(
+    task: TaskResponse,
+    onItemClick: () -> Unit,
+    onDeleteClick: () -> Unit // New parameter for delete action
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -87,7 +98,23 @@ fun TaskItem(task: TaskResponse, onItemClick: () -> Unit) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(text = "Title: ${task.title}")
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "Title: ${task.title}",
+                    Modifier.weight(1f)
+                )
+                IconButton(onClick = {
+                    onDeleteClick()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete Task"
+                    )
+                }
+            }
             task.description?.let { description ->
                 Text(text = "Description: $description")
             }
