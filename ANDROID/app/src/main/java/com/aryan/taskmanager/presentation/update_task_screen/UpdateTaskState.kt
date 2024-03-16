920fdeef2c39e70944b2866effc5f519bbb9426c
Aryan Singh <125189374+mrysg@users.noreply.github.com>
2024-03-16 12:55:09 +0530

    Minor Chnages

--- ANDROID/app/src/main/java/com/aryan/taskmanager/presentation/update_task_screen/UpdateTaskState.kt
+++ ANDROID/app/src/main/java/com/aryan/taskmanager/presentation/update_task_screen/UpdateTaskState.kt
@@ -1,9 +1,10 @@
 package com.aryan.taskmanager.presentation.update_task_screen
 
 data class UpdateTaskState(
-    val titleState: String = "",
-    val descriptionState: String = "",
-    val dueDateState: String = "",
+    val titleState: String = "Hackathon",
+    val descriptionState: String = "Develop useful skill for hackathon. " +
+            "Frontend- Have to complete HTML, CSS & JavaScript." ,
+    val dueDateState: String = "31-March-2024",
     val priorityState: Int = 1,
     val isDoneState: Boolean = false,
     val isLoading: Boolean = false
